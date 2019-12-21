08b8d81a6defeafc7b39378896e30aff99640dcf
Phillip Maier <pmaier983@gmail.com>
2019-12-21 09:47:24 -0500

    Apollo Queries

---
+++ src/Queries.js
@@ -0,0 +1,41 @@
+import { gql } from 'apollo-boost';
+
+const GET_EMAIL = gql`
+  query {
+    repository(owner:"octocat", name:"Hello-World") {
+      issues(last:20, states:CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first:5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const MY_REPOSITORY_LIST = gql`
+  query {
+    user(login: "pmaier983") {
+      id
+      repositories(first: 3) {
+        edges {
+          node {
+            name
+            createdAt
+          }
+        }
+      }
+    }
+  }
+`;
+
+export { GET_EMAIL, MY_REPOSITORY_LIST };
