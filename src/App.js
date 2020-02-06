4945fb9b81d44b375be51bf7294e5b55dea68e74
Phillip Maier <pmaier983@gmail.com>
2020-02-06 15:28:10 -0500

    Add button types and undo hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useQuery } from './hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { MY_REPOSITORY_LIST, GET_EMAIL, BAD_EMAIL } from './Queries';
 
 const BodyWidth = styled.div`
   width: 100%;
   height: 100%;
-`
+`;
 
 const QueryProvider = () => {
-  const [query, setQuery] = useState(MY_REPOSITORY_LIST)
+  const [query, setQuery] = useState(MY_REPOSITORY_LIST);
   const { data } = useQuery(query);
 
   const setGoodQuery = () => {
-    setQuery(GET_EMAIL)
-  }
+    setQuery(GET_EMAIL);
+  };
 
   const setBadQuery = () => {
-    setQuery(BAD_EMAIL)
-  }
+    setQuery(BAD_EMAIL);
+  };
 
   return (
     <div>
       <h1>Queries:</h1>
 
-      <button onClick={setGoodQuery}>Click Me to Fire off a Good Query</button>
-      <button onClick={setBadQuery}>Click Me to Fire off a Bad Query</button>
-      
+      <button type="button" onClick={setGoodQuery}>Click Me to Fire off a Good Query</button>
+      <button type="button" onClick={setBadQuery}>Click Me to Fire off a Bad Query</button>
+
       <h1>Output:</h1>
       <BodyWidth>{JSON.stringify(data)}</BodyWidth>
-      
+
     </div>
   );
 };
